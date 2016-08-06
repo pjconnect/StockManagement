c3408ce3946a6f5ec8c43570cf99f2203ee5d565
kitzerland <kitz.kitzerland@gmail.com>
2016-08-06 23:09:01 +0530

    001 design

---
+++ StockManagemengBasic/AddUsers.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace StockManagemengBasic
+{
+    partial class AddUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateUser = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cmbUserLevel = new System.Windows.Forms.ComboBox();
+            this.dgUsers = new System.Windows.Forms.DataGridView();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreateUser
+            // 
+            this.btnCreateUser.Location = new System.Drawing.Point(240, 94);
+            this.btnCreateUser.Name = "btnCreateUser";
+            this.btnCreateUser.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateUser.TabIndex = 0;
+            this.btnCreateUser.Text = "Create User";
+            this.btnCreateUser.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(174, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // cmbUserLevel
+            // 
+            this.cmbUserLevel.FormattingEnabled = true;
+            this.cmbUserLevel.Location = new System.Drawing.Point(141, 40);
+            this.cmbUserLevel.Name = "cmbUserLevel";
+            this.cmbUserLevel.Size = new System.Drawing.Size(174, 21);
+            this.cmbUserLevel.TabIndex = 2;
+            // 
+            // dgUsers
+            // 
+            this.dgUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgUsers.Location = new System.Drawing.Point(321, 13);
+            this.dgUsers.Name = "dgUsers";
+            this.dgUsers.Size = new System.Drawing.Size(355, 261);
+            this.dgUsers.TabIndex = 4;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(12, 16);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(55, 13);
+            this.lbl1.TabIndex = 5;
+            this.lbl1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "User Level";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(174, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // AddUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 330);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.dgUsers);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.cmbUserLevel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCreateUser);
+            this.Name = "AddUsers";
+            this.Text = "AddUsers";
+            ((System.ComponentModel.ISupportInitialize)(this.dgUsers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateUser;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cmbUserLevel;
+        private System.Windows.Forms.DataGridView dgUsers;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ StockManagemengBasic/AddUsers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StockManagemengBasic
+{
+    public partial class AddUsers : Form
+    {
+        public AddUsers()
+        {
+            InitializeComponent();
+            cmbUserLevel.Items.Add("Admin");
+            cmbUserLevel.Items.Add("User");
+        }
+    }
+}
