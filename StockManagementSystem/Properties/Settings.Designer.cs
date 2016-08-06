49f3d2b0c3b3f3d019d89fbe1a2e4e4af8359c4d
kitzerland <kitz.kitzerland@gmail.com>
2016-08-06 20:23:57 +0530

    vs2010 compatible

--- StockManagementSystem/Properties/Settings.Designer.cs
+++ StockManagementSystem/Properties/Settings.Designer.cs
@@ -8,29 +8,22 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace StockManagemengBasic.Properties {
-    
-    
+namespace StockManagementSystem.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=10.44.107.12;Initial Catalog=tmppasindu;User ID=sa;Password=ES@teamwo" +
-            "rk")]
-        public string tmppasinduConnectionString {
-            get {
-                return ((string)(this["tmppasinduConnectionString"]));
             }
         }
     }
